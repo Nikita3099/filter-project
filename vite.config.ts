c4c887d011f55da8160239d018b3159378c15839
Nikita3099 <n39335875@gmail.com>
2026-02-20 16:42:23 +0300

    Revert index.html path to main.tsx for Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/filter-project/',
-  build: {
-    rollupOptions: {
-      external: [/^\/src\/main\.tsx$/]  
-    }
-  }
+  base: '/filter-project/'
 })
